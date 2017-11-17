26a8003d985820f0e4a6d5a018d486c74dda34ea
Yasuhisa Yoshida <syou6162@gmail.com>
2017-11-17 19:49:18 +0900

    学習データを使って学習済みのモデルファイルを出力するスクリプトを追加

---
+++ train_perceptron.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/syou6162/saba_disambiguator/lib"
+)
+
+func parseLine(line string) (twitter.Tweet, error) {
+	var tweet twitter.Tweet
+	err := json.Unmarshal([]byte(line), &tweet)
+	return tweet, err
+}
+
+func readExamplesFromFile(fileName string, label sabadisambiguator.LabelType) (sabadisambiguator.Examples, error) {
+	var examples sabadisambiguator.Examples
+	fp, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		t, err := parseLine(text)
+		if err != nil {
+			continue
+		}
+		e := sabadisambiguator.NewExample(t, label)
+		examples = append(examples, e)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return examples, nil
+}
+
+func main() {
+	examplesPos, err := readExamplesFromFile(os.Args[1], sabadisambiguator.POSITIVE)
+	if err != nil {
+		panic(err)
+	}
+
+	examplesNeg, err := readExamplesFromFile(os.Args[2], sabadisambiguator.NEGATIVE)
+	if err != nil {
+		panic(err)
+	}
+
+	examples := append(examplesPos, examplesNeg...)
+	p := sabadisambiguator.NewPerceptronClassifier(examples)
+	sabadisambiguator.WritePerceptron(*p, "model/model.bin")
+}
