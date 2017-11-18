7e7867cf27934fe1f7f8a9b6b269cf91095e5832
Yasuhisa Yoshida <syou6162@gmail.com>
2017-11-19 04:29:35 +0900

    特徴量追加

--- lib/feature.go
+++ lib/feature.go
@@ -29,6 +29,10 @@ func extractJpnNounFeatures(s string, prefix string) FeatureVector {
 	return fv
 }
 
+func inReplyToScreenName(t twitter.Tweet) string {
+	return t.InReplyToScreenName
+}
+
 func containsMackerelInScreenName(screenName string) bool {
 	return strings.Contains(strings.ToLower(screenName), "mackerel")
 }
@@ -43,6 +47,10 @@ func includeMackerelInUserMentions(t twitter.Tweet) bool {
 	return result
 }
 
+func includeMackerelInReplyToScreenName(t twitter.Tweet) bool {
+	return containsMackerelInScreenName(t.InReplyToScreenName)
+}
+
 func lang(t twitter.Tweet) string {
 	return t.Lang
 }
@@ -51,16 +59,71 @@ func ExtractNounFeatures(s string, prefix string) FeatureVector {
 	return extractJpnNounFeatures(s, prefix)
 }
 
+func extractNounFeaturesFromQuotedText(t twitter.Tweet) FeatureVector {
+	var fv FeatureVector
+	if t.QuotedStatus == nil {
+		return fv
+	}
+	return ExtractNounFeatures(t.QuotedStatus.Text, "QuotedText")
+}
+
+func extractNounFeaturesFromUserDescription(t twitter.Tweet) FeatureVector {
+	var fv FeatureVector
+	if t.QuotedStatus == nil {
+		return fv
+	}
+	return ExtractNounFeatures(t.User.Description, "UserDEscription")
+}
+
+func screenNameInQuotedStatus(t twitter.Tweet) string {
+	result := ""
+	if t.QuotedStatus == nil {
+		return result
+	}
+	return t.QuotedStatus.User.ScreenName
+}
+
+func domainsInEntities(t twitter.Tweet) FeatureVector {
+	var fv FeatureVector
+	if t.Entities == nil {
+		return fv
+	}
+
+	for _, u := range t.Entities.Urls {
+		fv = append(fv, "DomainsInEntity:"+strings.Split(u.DisplayURL, "/")[0])
+	}
+	return fv
+}
+
+func hashtagsInEntities(t twitter.Tweet) FeatureVector {
+	var fv FeatureVector
+	if t.Entities == nil {
+		return fv
+	}
+
+	for _, h := range t.Entities.Hashtags {
+		fv = append(fv, "HashtagsInEntity:"+h.Text)
+	}
+	return fv
+}
+
 func ExtractFeatures(t twitter.Tweet) FeatureVector {
 	var fv FeatureVector
 	text := t.Text
 
 	fv = append(fv, "BIAS")
 	fv = append(fv, "ScreenName:"+t.User.ScreenName)
+	fv = append(fv, "inReplyToScreenName:"+inReplyToScreenName(t))
+	fv = append(fv, "screenNameInQuotedStatus"+screenNameInQuotedStatus(t))
 	fv = append(fv, "lang:"+lang(t))
 	fv = append(fv, "containsMackerelInScreenName:"+strconv.FormatBool(containsMackerelInScreenName(t.User.ScreenName)))
 	fv = append(fv, "includeMackerelInUserMentions:"+strconv.FormatBool(includeMackerelInUserMentions(t)))
+	fv = append(fv, "includeMackerelInReplyToScreenName:"+strconv.FormatBool(includeMackerelInReplyToScreenName(t)))
 
-	fv = append(fv, ExtractNounFeatures(text, "BODY")...)
+	fv = append(fv, ExtractNounFeatures(text, "Text")...)
+	fv = append(fv, extractNounFeaturesFromQuotedText(t)...)
+	fv = append(fv, extractNounFeaturesFromUserDescription(t)...)
+	fv = append(fv, domainsInEntities(t)...)
+	fv = append(fv, hashtagsInEntities(t)...)
 	return fv
 }
