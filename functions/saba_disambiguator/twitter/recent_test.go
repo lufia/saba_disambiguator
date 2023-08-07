b6584057d7135117b1fbe653e3cf0ef25b028e2d
rmatsuoka <76464810+rmatsuoka@users.noreply.github.com>
2023-08-07 18:53:24 +0900

    -conn があるときだけ twitter へ接続するテストをする

--- functions/saba_disambiguator/twitter/recent_test.go
+++ functions/saba_disambiguator/twitter/recent_test.go
@@ -1,11 +1,17 @@
 package twitter2
 
 import (
+	"flag"
 	"os"
 	"testing"
 )
 
+var doConnectionTest = flag.Bool("conn", false, "twitter に接続したテストをする")
+
 func TestRecentTweet(t *testing.T) {
+	if !*doConnectionTest {
+		t.Skip("-conn が指定されていないので skip")
+	}
 	c := NewClient(os.Getenv("BEARER_TOKEN"))
 	tweets, err := c.RecentSearch("mackerel lang:ja -is:retweet")
 	for _, tweet := range tweets {
